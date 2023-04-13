70090bded67acd2d233030ff8328fe7b099f9cd4
hiepxuan <hiepxuan2006@gmail.com>
2023-04-13 23:54:54 +0700

    push

--- src/connections/redisConnection.js
+++ src/connections/redisConnection.js
@@ -1,10 +1,11 @@
 const redis = require("redis")
 
 module.exports.connectRedis = async () => {
-  const url = process.env.REDIS_URI
+  const url =
+    "redis://redis-18599.c295.ap-southeast-1-1.ec2.cloud.redislabs.com:18599"
   const client = redis.createClient({
     url,
-    password: process.env.REDIS_KEY,
+    password: "bx4cf6SHDfH7blNjCB2UyVBk3HUBrqEv",
   })
   client.on("connect", () => console.log("DB redis connected"))
   client.on("error", (err) => console.log("client Connection Error", err))
