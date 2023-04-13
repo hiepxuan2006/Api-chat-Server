1106f0a779dc16e2ce80110c6ece1d81a0e5debc
hiepxuan <hiepxuan2006@gmail.com>
2023-04-13 23:28:43 +0700

    up

--- src/connections/database.js
+++ src/connections/database.js
@@ -4,6 +4,8 @@ const createStore = require("../schemas")
 const MONGODB_URI = process.env.MONGODB_URI
 console.log("MONGODB_URI:", MONGODB_URI)
 
-const originConnection = createMongoConnection(MONGODB_URI)
+const originConnection = createMongoConnection(
+  "mongodb+srv://hiepxuan2006:Hiepxuan2006@cluster0.pzzy0qz.mongodb.net/media?retryWrites=true&w=majority"
+)
 
 module.exports = createStore(originConnection)
--- src/connections/db.js
+++ src/connections/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose")
 module.exports.connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    await mongoose.connect(
+      "mongodb+srv://hiepxuan2006:Hiepxuan2006@cluster0.pzzy0qz.mongodb.net/media?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    )
 
     console.log("MongoDB connected")
   } catch (error) {
